b8741fe807793b7829646c22e5677beee6fe7c9e
Robert Dunn <robertdunn220@gmail.com>
2017-01-05 13:02:59 -0500

    Finished division opponents

--- mong_server.js
+++ mong_server.js
@@ -19,7 +19,7 @@ const Team = mongoose.model("Team", {
 });
 
 app.get('/api/all_teams', function(req, res) {
-  Team.find()
+  Team.find().sort({'-schedule.week': -1})
   .then(function(teams) {
     res.json(
       teams
@@ -28,48 +28,41 @@ app.get('/api/all_teams', function(req, res) {
 });
 
 app.get('/api/specific_teams', function(req, res) {
-  var queryPrms = req.query.prms;
-
-  if (queryPrms === 'name') {
-    Team.find({name: req.query.name})
-    .then(function(teams) {
-      res.json(
-        teams
-      );
-    });
-  }
-
-  else if (queryPrms === 'division') {
-    Team.find({division: req.query.name})
-    .then(function(teams) {
-      res.json(
-        teams
-      );
-    });
-  }
+  console.log(req.query.oppName);
+  Team.find({name: {
+    $in: [req.query.oppName, req.query.name]}
+  })
+  .then(function(teams) {
+    res.json(
+      teams
+    );
+  });
 });
 
 app.post('/api/update_sched', function(req, res) {
-  Team.update(
-    {name: req.body.team},
-    {
-      $push: {
-        schedule:
-          { week: req.body.week, opponent: req.body.opponent }
-      },
-      $pull: {
-        weeksLeft:
-          { $in:
-            [req.body.week]
-          }
+  console.log(req.body.teams);
+  for (var i = 0; i < req.body.teams.length; i++) {
+    Team.update(
+      {name: req.body.teams[i].team},
+      {
+        $push: {
+          schedule:
+            { week: req.body.teams[i].week, opponent: req.body.teams[i].opponent }
+        },
+        $pull: {
+          weeksLeft:
+            { $in:
+              [req.body.teams[i].week]
+            }
+        }
       }
-    }
-  ).then(function(result) {
-    console.log('Success');
-  })
-  .catch(function(err) {
-    console.log('Error', err.message);
-  });
+    ).then(function(result) {
+
+    })
+    .catch(function(err) {
+
+    });
+  }
 });
 
 // Team.update(
@@ -87,84 +80,46 @@ app.post('/api/update_sched', function(req, res) {
 //     console.log('Error', err.message);
 //   });
 
+// Team.create({
+//   name: 'Dallas Cowboys',
+//   division: 'NFC East',
+//   conference: 'NFC',
+//   weeksLeft: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17],
+//   schedule: []
+// }).then(function(result) {
+//   console.log('Success');
+// });
+//
+// Team.create({
+//   name: 'New York Giants',
+//   division: 'NFC East',
+//   conference: 'NFC',
+//   weeksLeft: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17],
+//   schedule: []
+// }).then(function(result) {
+//   console.log('Success');
+// });
+//
+// Team.create({
+//   name: 'Philadelphia Eagles',
+//   division: 'NFC East',
+//   conference: 'NFC',
+//   weeksLeft: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17],
+//   schedule: []
+// }).then(function(result) {
+//   console.log('Success');
+// });
+//
+// Team.create({
+//   name: 'Washington Redskins',
+//   division: 'NFC East',
+//   conference: 'NFC',
+//   weeksLeft: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17],
+//   schedule: []
+// }).then(function(result) {
+//   console.log('Success');
+// });
 
 app.listen(3000, function() {
   console.log('Listening on 3000');
 });
-
-// Team.create({
-//   name: 'Atlanta Falcons',
-//   division: 'NFC South',
-//   conference: 'NFC',
-//   weeksLeft: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17],
-//   schedule: [{
-//   }]
-// });
-
-// User.create({
-//   _id: 'guy',
-//   website: 'www.buddy.com',
-//   avatar_url: '@notyourfriend'
-// });
-
-// Follow.create(
-//   {
-//     follower: 'robdunn220',
-//     following: 'guy'
-//   }
-// )
-// .then(function(res) {
-//   console.log(res.follower + ' is following', res.following);
-// })
-// .catch(function(err) {
-//   console.log('Error: ', err.message);
-// });
-
-// Tweet.create({
-//   text: 'Im not your buddy, pal!',
-//   timestamp: new Date(),
-//   userID: 'guy'
-// })
-// .then(function(res) {
-//   console.log(res);
-// });
-
-// world timeline
-
-// Tweet.find()
-// .then(function(tweets) {
-//   console.log('Timeline: ', tweets);
-// });
-//
-// // user profile page
-//
-
-// var theUserID = 'robdunn220';
-//
-// bluebird.all([
-//   User.findById(theUserID),
-//   Tweet.find({ userID: theUserID })
-//   .then(function(tweets) {
-//     console.log(tweets);
-//   })
-// ]);
-
-//
-// // your timeline
-// //
-// var theUserID = 'guy';
-// Follow.find({ follower: theUserID })
-//   .then(function(follows) {
-//     var followingIds = follows.map(function(follow) {
-//       return follow.following;
-//     });
-//     // find all following's tweets
-//     return Tweet.find({
-//       userID: {
-//         $in: followingIds.concat([theUserID])
-//       }
-//     });
-//   })
-//   .then(function(tweets) {
-//     console.log(tweets);
-//   });
--- public/entireScheduleGen.js
+++ public/entireScheduleGen.js
@@ -10,25 +10,23 @@ app.factory("NFL_Api", function factoryFunction($http, $rootScope) {
     });
   };
 
-  service.searchForOpponents = function(param, term) {
+  service.singleTeam = function(currName, oppName) {
     return $http({
       url: '/api/specific_teams',
       method: 'GET',
       params: {
-        prms: param,
-        name: term
+        name: currName,
+        oppName: oppName
       }
     });
   };
 
-  service.updateTeamSchedule = function(team, week, opponent) {
+  service.updateTeamSchedule = function(teams) {
     return $http({
       url: '/api/update_sched',
       method: 'POST',
       data: {
-        team: team,
-        week: week,
-        opponent: opponent
+        teams: teams
       }
     });
   };
@@ -64,84 +62,171 @@ app.controller('AllTeamsController', function($scope, NFL_Api) {
 });
 
 app.controller('ScheduleGeneratorController', function($scope, NFL_Api, $rootScope, $cookies, $stateParams) {
+  $scope.divNames = [];
+  $scope.teamSchedule = [];
 
-  $scope.firstPop = function(callback) {
-    NFL_Api.displayAllTeams().success(function(results) {
-      callback(results);
-    });
+  $scope.getAllTeams = function(callback) {
+      NFL_Api.displayAllTeams().success(function(results) {
+        $scope.allTeams = results;
+        callback();
+      });
   };
 
-  $scope.entireSchedule = function(allTeams) {
-    var currentTeam = allTeams[0];
-    var divisionOpps = [];
-    var weekPickedCurrTeam, weekPickedOtherTeam;
-    var divCount = 0;
+  $scope.displaySchedule = function () {
 
-    // var divSchedule = function(teams, currTeam, divCount) {
-    //
-    //     $scope.updateSchedule = function(team, week, opponent, dC, callback) {
-    //
-    //       $scope.updateInfo = function(callback, team, dC) {
-    // NFL_Api.displayAllTeams().success(function(results) {
-    //   console.log('3rd occurence', dC);
-    //   callback(results, team, dC);
-    // });
-    //       };
-    //       $scope.updateInfo(callback, team, dC);
-    //     };
-    // teams.forEach(function(team) {
-    //   if (team.division === currTeam.division && team.name !== currTeam.name) {
-            // var randNum = (Math.floor(Math.random() * (currTeam.weeksLeft.length - 1)));
-            // weekPickedCurrTeam = currTeam.weeksLeft[randNum];
-    //         divCount++;
-    //         console.log(team.name);
-    //         console.log('1st occurence', divCount);
-    //         $scope.updateSchedule(currTeam, weekPickedCurrTeam, team.name, divCount, divSchedule);
-    //       }
-    //     });
-    // };
-    var scheduleOpps = function(opponent, currentTeam, callback) {
-      var randNum = (Math.floor(Math.random() * (currentTeam.weeksLeft.length - 1)));
-      weekPickedCurrTeam = currentTeam.weeksLeft[randNum];
-      var update = function(callback) {
-          NFL_Api.displayAllTeams().success(function(data) {
-            callback(data);
-          });
-      };
+  };
 
-      update(callback);
-      NFL_Api.updateTeamSchedule(currentTeam.name, weekPickedCurrTeam, opponent.name).success(function(results) {
-      });
-      NFL_Api.updateTeamSchedule(opponent.name, weekPickedCurrTeam, currentTeam.name).success(function(results) {
-      });
-    };
+  $scope.createSchedule = function() {
 
-    var divDone = [];
-    var divSchedule = function(allTeams) {
-      console.log(currentTeam);
-      allTeams.forEach(function(team) {
-        if (team.division === currentTeam.division && team.name !== currentTeam.name) {
-          if (!divDone[team.name]) {
-            divDone[team.name] = 1;
-            scheduleOpps(team, currentTeam, divSchedule);
-          }
-          else if (divDone[team.name] === 1) {
-            divDone[team.name] = 2;
-            scheduleOpps(team, currentTeam, divSchedule);
+    $scope.divOpps = [];
+    $scope.otherDivOpps = [];
+    $scope.divPairs = [];
+    $scope.pairOneTeams = [];
+
+    if (!$scope.divDone) {
+      for (var x = 0; x < $scope.allTeams.length; x++) {
+        if ($scope.allTeams[x].division === $scope.allTeams[$scope.divNum].division) {
+          $scope.divOpps.push($scope.allTeams[x]);
+        }
+      }
+      $scope.divNames.push($scope.allTeams[$scope.divNum].division);
+
+      $scope.divOpps.forEach(function(team) {
+        for (var i = 0; i < $scope.divOpps.length; i++) {
+          if (team.name !== $scope.divOpps[i].name) {
+            $scope.match = false;
+            var index;
+            var randNum = (Math.floor(Math.random() * (team.weeksLeft.length - 1)));
+            for (var o = 0; o < $scope.divOpps[i].weeksLeft.length; o++) {
+              if (team.weeksLeft[randNum] === $scope.divOpps[i].weeksLeft[o]) {
+                $scope.match = true;
+                weekPickedCurrTeam = team.weeksLeft[randNum];
+                weekPickedOtherTeam = $scope.divOpps[i].weeksLeft[o];
+                index = o;
+              }
+            }
+
+            if ($scope.match === true) {
+              $scope.teamSchedule.push({'team': team.name, 'week': weekPickedCurrTeam, 'opponent': $scope.divOpps[i].name});
+              $scope.teamSchedule.push({'team': $scope.divOpps[i].name, 'week': weekPickedOtherTeam, 'opponent': team.name});
+              team.weeksLeft.splice(randNum, 1);
+              $scope.divOpps[i].weeksLeft.splice(index, 1);
+            }
+
+            else {
+              for (var a = 0; a < $scope.divOpps[i].weeksLeft.length; a++) {
+                for (var b = 0; b < team.weeksLeft.length; b++) {
+                  if (team.weeksLeft[b] === $scope.divOpps[i].weeksLeft[a]) {
+                    weekPickedCurrTeam = team.weeksLeft[b];
+                    weekPickedOtherTeam = $scope.divOpps[i].weeksLeft[a];
+                    $scope.teamSchedule.push({'team': team.name, 'week': weekPickedCurrTeam, 'opponent': $scope.divOpps[i].name});
+                    $scope.teamSchedule.push({'team': $scope.divOpps[i].name, 'week': weekPickedOtherTeam, 'opponent': team.name});
+                    team.weeksLeft.splice(b, 1);
+                    $scope.divOpps[i].weeksLeft.splice(a, 1);
+                    a = $scope.divOpps[i].weeksLeft.length;
+                    b = team.weeksLeft.length;
+                  }
+                }
+              }
+            }
           }
         }
       });
-    };
 
-    divSchedule(allTeams);
 
-    $cookies.put('generated', true);
+      $scope.divNum += 4;
+
+      if ($scope.divNum >= $scope.allTeams.length) {
+        $scope.divDone = true;
+        $scope.otherDiv = true;
+      }
+    }
+
+    if ($scope.otherDiv) {
+      for (var i = 0; i < $scope.divNames.length; i+=2) {
+        $scope.divPairs.push([$scope.divNames[i], $scope.divNames[i+1]]);
+      }
+
+      for (var c = 0; c < $scope.divPairs.length; c++) {
+        $scope.pairOneTeams[c] = [];
+        for (var d = 0; d < $scope.divPairs[c].length; d++) {
+          $scope.pairOneTeams[c][d] = [];
+          for (var e = 0; e < $scope.allTeams.length; e++) {
+            if ($scope.allTeams[e].division === $scope.divPairs[c][d]) {
+              $scope.pairOneTeams[c][d].push($scope.allTeams[e]);
+            }
+          }
+        }
+      }
+
+      var otherDivSchedule = function(divArr) {
+        divArr.forEach(function(group) {
+          group[0].forEach(function(teamOne) {
+            group[1].forEach(function(teamTwo) {
+              $scope.match = false;
+              console.log('Team one weeks left:', teamOne.weeksLeft);
+              var index;
+              var randNum = (Math.floor(Math.random() * (teamOne.weeksLeft.length - 1)));
+              for (var o = 0; o < teamTwo.weeksLeft.length; o++) {
+                if (teamOne.weeksLeft[randNum] === teamTwo.weeksLeft[o]) {
+                  $scope.match = true;
+                  weekPickedCurrTeam = teamOne.weeksLeft[randNum];
+                  weekPickedOtherTeam = teamTwo.weeksLeft[o];
+                  index = o;
+                }
+              }
+
+              if ($scope.match === true) {
+                $scope.teamSchedule.push({'team': teamOne.name, 'week': weekPickedCurrTeam, 'opponent': teamTwo.name});
+                $scope.teamSchedule.push({'team': teamTwo.name, 'week': weekPickedOtherTeam, 'opponent': teamOne.name});
+                teamOne.weeksLeft.splice(randNum, 1);
+                teamTwo.weeksLeft.splice(index, 1);
+              }
+
+              else {
+                for (var a = 0; a < teamTwo.weeksLeft.length; a++) {
+                  for (var b = 0; b < teamOne.weeksLeft.length; b++) {
+                    if (teamOne.weeksLeft[b] === teamTwo.weeksLeft[a]) {
+                      weekPickedCurrTeam = teamOne.weeksLeft[b];
+                      weekPickedOtherTeam = teamTwo.weeksLeft[a];
+
+                      $scope.teamSchedule.push({'team': teamOne.name, 'week': weekPickedCurrTeam, 'opponent': teamTwo.name});
+                      $scope.teamSchedule.push({'team': teamTwo.name, 'week': weekPickedOtherTeam, 'opponent': teamOne.name});
+                      teamOne.weeksLeft.splice(b, 1);
+                      teamTwo.weeksLeft.splice(a, 1);
+                      a = teamTwo.weeksLeft.length;
+                      b = teamOne.weeksLeft.length;
+                    }
+                  }
+                }
+              }
+
+              console.log('Team one week picked:', weekPickedCurrTeam);
+              console.log('Team one weeks left:', teamOne.weeksLeft);
+            });
+          });
+        });
+      };
+      otherDivSchedule($scope.pairOneTeams);
+      console.log($scope.teamSchedule);
+      // NFL_Api.updateTeamSchedule($scope.teamSchedule).success(function(result) {});
+    }
+
+
+
   };
 
-  if (!$cookies.put('generated')) {
-    $scope.firstPop($scope.entireSchedule);
+  if (!$scope.madeDiv) {
+    $scope.madeDiv = 1;
+    $scope.divNum = 0;
+    $scope.getAllTeams($scope.createSchedule);
   }
 
+  while ($scope.madeDiv < 4) {
+
+    $scope.madeDiv += 1;
+    $scope.getAllTeams($scope.createSchedule);
+  }
 });
 
 app.config(function($stateProvider, $urlRouterProvider) {
@@ -154,7 +239,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
     })
     .state({
       name : 'schedule_generator',
-      url : '/create_schedule/{team}',
+      url : '/create_schedule',
       templateUrl: 'sched_gen.html',
       controller: 'ScheduleGeneratorController'
     });

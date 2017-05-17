1f605abb71a523393e43b0e5d94f567062288b4a
Robert <robertdunn220@gmail.com>
2017-05-17 13:09:29 -0400

    Fixed Mongoose package

--- public/entireScheduleGen.js
+++ public/entireScheduleGen.js
@@ -61,52 +61,7 @@ app.controller('AllTeamsController', function($scope, NFL_Api) {
 });
 
 app.controller('ScheduleGeneratorController', function($scope, NFL_Api) {
-  $scope.teamSched = [];
-  $scope.count = 0;
-
-  $scope.sched_gen = function() {
-
-    if ($scope.count < 16) {
-      NFL_Api.displayAllTeams().success(function(results) {
-        // The teams are stored in a $scope variable, and sorted into the two arrays using the $scope.sortTeams method
-        $scope.allTeams = results;
-
-        $scope.divOpps($scope.allTeams[$scope.count]);
-      });
-    }
-  };
-
-  $scope.divOpps = function(team) {
-
-    console.log(team.name);
-    $scope.allTeams.forEach(function(otherTeam) {
-
-      if (otherTeam.division === team.division && otherTeam.name !== team.name) {
-        $scope.teamSched.push({'team': team.name, 'week': 1, 'opponent': otherTeam.name});
-        $scope.teamSched = [];
-        $scope.sched_gen();
-        // NFL_Api.updateTeamSchedule($scope.teamSched).success(function(result) {});
-      }
-    });
-
-    console.log('Count', $scope.count);
-    $scope.count++;
-  };
-
-  // $scope.otherDivOpps = function(team) {
-  //   console.log(team.name);
-  //   $scope.count++;
-  //   $scope.allTeams.forEach(function(otherTeam) {
-  //     if (otherTeam.division !== team.division && otherTeam.conference === team.conference) {
-  //       $scope.teamSched.push({'team': team.name, 'week': 1, 'opponent': otherTeam.name});
-  //     }
-  //   });
-  //
-  //   // NFL_Api.updateTeamSchedule($scope.teamSched).success(function(result) {});
-  //   $scope.teamSched = [];
-  // };
-
-  $scope.sched_gen();
+  
 });
 
 app.config(function($stateProvider, $urlRouterProvider) {
